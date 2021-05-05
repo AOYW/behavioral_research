e4339c5395fec9dd2cf37ebeeb3f4daf52aa8e84
AOYW <45650873+AOYW@users.noreply.github.com>
2021-05-05 13:38:26 -0700

    Update and rename timeline_survey/timeline_qualtrics_js.js to CTB_survey/timeline_qualtrics_js.js
    Moved to CTB folder and changed timeline scale to age 0 to 85

--- CTB_survey/timeline_qualtrics_js.js
+++ CTB_survey/timeline_qualtrics_js.js
@@ -6,13 +6,14 @@ Qualtrics.SurveyEngine.addOnload(function()
 	/*Place your JavaScript here to run when the page loads*/
 	
 	/* variable to be captured from survey */
-	var currentAge = 22
+	var currentAge = parseInt("${e://Field/currentAge}") || 21;
 	
-	/* size of the graphic */
+	/* size of the graphic and style parameters */
 	var height = 300;
 	var width = 800;
-	var margin = 100; // left right margin
+	var margin = 60; // left right margin
 	var maxAge = 85;
+	var labelSize = 16; // font-size of labels
 	
 	/* create svg element*/
 	var svg = d3.select("#res")
@@ -22,12 +23,13 @@ Qualtrics.SurveyEngine.addOnload(function()
 	
 	/* create scales for years and ages */
 	var years = d3.scaleTime()
-		.domain([Date.now(), Date.now() + (maxAge - currentAge) * 365.24 * 24 * 60 * 60 * 1000])
+		.domain([Date.now() - currentAge * 365.24 * 24 * 60 * 60 * 1000, 
+				 	   Date.now() + (maxAge - currentAge) * 365.24 * 24 * 60 * 60 * 1000])
 		.range([margin, width -  margin])
 		//.nice();
 	
 	var ages = d3.scaleLinear()
-		.domain([currentAge, maxAge])
+		.domain([0, maxAge])
 		.range([margin, width - margin])
 		//.nice();
 
@@ -41,53 +43,85 @@ Qualtrics.SurveyEngine.addOnload(function()
 		 .attr("class", "x axis")
 		 .attr("transform", "translate(0," + (height/2 + 10) + ")")
 		 .call(d3.axisBottom(ages));
-
+	
+	/* adding text labels */
 	svg.append("text")
 		.attr("text-anchor", "end")
-		.attr("x", width - 50)
+		.attr("x", width - margin)
 		.attr("y", height/2 - 50)
-		.text("Year");
+		.attr('dominant-baseline', 'middle')
+		.text("Date");
 	
 	svg.append("text")
 		.attr("text-anchor", "end")
-		.attr("x", width - 50)
-		.attr("y", height/2 + 60)
+		.attr("x", width - margin)
+		.attr("y", height/2 + 50)
+		.attr('dominant-baseline', 'middle')
 		.text("Age");
+	
+	svg.append("text")
+		.attr("text-anchor", "start")
+		.attr("x", 0)
+		.attr("y", height/2)
+		.attr('dominant-baseline', 'middle')
+		.text("Now");
 	
 	function renderPoint(time, text, isLate) {
 		/* find x coordinates of the time point*/
 		var xAge = ages(currentAge + time);
 		var xYear = years(Date.now() + time * 365.24 * 24 * 60 * 60 * 1000);
+		/* add additional text to display */
+		text += "\nIt is the year " + (parseInt(new Date().getFullYear()) + time);
+		text += "\nand you are " + (currentAge + time) + " years old ";
 		/* define the color to use */
 		var color = (isLate === true) ? '#80b1d3' : '#fb8072';
+		var yStart = (isLate === true) ? (height/2 - 10) : (height/2 + 10);
+		var yEnd= (isLate === true) ? height - 80 : 80;
 		
-		/* draw circles at the point */
-		svg.append("circle") // add circle for age axis
-			.attr("transform", "translate(0, " + (height/2 + 10) + ")")
-			.attr("cx", xAge)
-			.attr("fill", color)
-			.attr("r", 10);
-		
-		svg.append("circle") // add circle for year axis
-			.attr("transform", "translate(0, " + (height/2 - 10) + ")")
-			.attr("cx", xYear)
-			.attr("fill", color)
-			.attr("r", 10);
+		if (isLate === false && text != 'now') {
+			/* draw a line at the now if the early period is not now */
+			svg.append("line")
+				.style('stroke', color)
+				.attr('stroke-width', '3')
+				.attr("x1", xYear)
+				.attr("y1", yStart)
+				.attr("x2", xAge)
+				.attr("y2", yEnd)
+			
+			/* label now*/
+			svg.append("text")
+				.attr("x", xAge)
+				.attr("y", 80)
+				.attr("text-anchor", "end")
+				.attr('dominant-baseline', 'middle')
+				.style("font-size", labelSize + 'px')
+				.style('fill', 'black')
+				.text('Now')
+		}
 		
 		/* draw a line at the point */
 		svg.append("line")
-			.style('stroke', color)
-			.style("stroke-dasharray", ("5, 5"))
-			.attr("x1", xYear)
-			.attr("y1", margin)
-			.attr("x2", xAge)
-			.attr("y2", height - margin)
+			.style('stroke', 'black')
+			.attr('stroke-width', '3')
+			.attr("x1", years(Date.now()))
+			.attr("y1", yStart)
+			.attr("x2", ages(currentAge))
+			.attr("y2", yEnd)
 		
-		svg.append("text") // label time points
-			.attr("x", xAge)
-			.attr("y", (isLate === true) ? height - 70 : 70)
-			.style('fill', color)
-			.text(text);
+		/* label timepoints with multiline text */
+		var texts = (isLate === true) ? text.split('\n') : text.split('\n').reverse();
+		var y = (isLate === true) ? height - 75 : 75;
+		var yOffset = (isLate === true) ? labelSize : -labelSize;
+		for (i = 0; i < texts.length; i++) { // loop for multiple lines
+			svg.append("text")
+				.attr("x", xAge)
+				.attr("y", y + i * yOffset)
+				.attr("text-anchor", "start")
+				.attr('dominant-baseline', 'middle')
+				.style("font-size", labelSize + 'px')
+				.style('fill', color)
+				.text(texts[i])
+		}
 	}
 	
 	renderPoint(parseInt("${lm://Field/3}"), "${lm://Field/1}", false) // mark the earlier date
